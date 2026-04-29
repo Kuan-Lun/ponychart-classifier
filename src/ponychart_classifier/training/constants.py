085e9222f8ed4c6ce2d2810bd677f38c1d078eb1
Kuan-Lun Wang <polyphonicared@gmail.com>
2026-04-29 13:21:25 +0800

    fix: Revert GOF_RECENT_ORIG_LIMIT to 1000 for consistency in balancing

--- src/ponychart_classifier/training/constants.py
+++ src/ponychart_classifier/training/constants.py
@@ -60,7 +60,7 @@ VAL_SIZE = 0.15
 
 # Balancing: goodness-of-fit significance level
 GOF_ALPHA = 0.05
-GOF_RECENT_ORIG_LIMIT = 2000
+GOF_RECENT_ORIG_LIMIT = 1000
 
 # Expected label-size ratios (single / double / triple)
 # Automatically normalized to probabilities; values only need to be proportional.
